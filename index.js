163127dcdbe8d2add2fc96b47582118cb614df34
cosen <rcosen@gmail.com>
2016-05-05 17:09:27 -0300

    Testanto github deploy

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get('/', function(request, response) {
 });
 
 app.get('/cool', function(request, response) {
-  response.send(cool());
+  response.send(cool() + " ------- " + cool());
 });
 
 app.get('/times', function(request, response) {

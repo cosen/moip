51ae601dc461ada7f14c70f3e9c5d6925a33485b
cosen <rcosen@gmail.com>
2016-05-07 19:20:49 -0300

    Definindo o build do gulp e organizando as pastas

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,62 @@
 var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var clean = require('gulp-clean');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var htmlmin = require('gulp-htmlmin');
+var cleanCSS = require('gulp-clean-css');
+var es = require('event-stream');
+var runSequence = require('run-sequence');
 
-gulp.task('default', function() {
-
+gulp.task('clean', function() {
+  return gulp.src('public').pipe(clean());
 });
+
+gulp.task('jshint', function() {
+  return gulp.src('src/js/*.js')
+  .pipe(jshint())
+  .pipe(jshint.reporter('default'));
+});
+
+gulp.task('uglify', function() {
+  return es.merge([
+    gulp.src([
+      'lib/angular/angular.min.js', 
+      'lib/angular-route/angular-route.min.js', 
+      'lib/angular-messages/angular-messages.min.js'
+    ]), 
+    gulp.src(['src/js/*.js'])
+    .pipe(concat('scripts.js'))
+    .pipe(uglify())
+  ])
+  .pipe(concat('all.min.js'))
+  .pipe(gulp.dest('public/js'));
+});
+
+gulp.task('htmlmin', function() {
+  return gulp.src('src/index.html')
+  .pipe(htmlmin({collapseWhitespace: true}))
+  .pipe(gulp.dest('public/'))
+});
+
+gulp.task('cssmin', function() {
+  return es.merge([
+    gulp.src([
+      'lib/bootstrap/dist/css/bootstrap.min.css' 
+    ]), 
+    gulp.src(['src/css/*.css'])
+    .pipe(concat('style.css'))
+    .pipe(cleanCSS())
+  ])
+  .pipe(concat('all.min.css'))
+  .pipe(gulp.dest('public/css'));
+});
+
+gulp.task('default', function(end) {
+  return runSequence(
+    'clean', 
+    ['jshint', 'uglify', 'htmlmin', 'cssmin'],
+    end);
+});
+
+

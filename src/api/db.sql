72d3ca6d1bea0e0d513ba6f4e2ba385f01049bb5
cosen <rcosen@gmail.com>
2016-05-09 17:06:58 -0300

    Enviando o script sql que roda no MySQL do Travis

---
+++ src/api/db.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `j2w1xpr3s01xy8ob`.`produtos` (
+  `id` BIGINT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(45) NOT NULL,
+  `descricao` VARCHAR(255) NOT NULL,
+  `preco` DOUBLE NOT NULL,
+  PRIMARY KEY (`id`));

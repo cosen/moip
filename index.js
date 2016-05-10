25e6d85286bd2b86a12ec4f50bff9c2a6cfe402e
cosen <rcosen@gmail.com>
2016-05-09 21:27:50 -0300

    Adicionando as rotas para as operações da API REST de produtos

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
-var express = require('express');
-var app     = express();
+var express    = require('express');
+var bodyParser = require('body-parser');
+var app        = express();
+
+var produtos = require('./src/api/produtos.js');
+
+// express e body-parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
+
+app.get('/produtos', function(req, res) {
+  produtos.findAll(function(error, results) {
+    if(error) {
+      console.log(error);
+      res.status(500).send('Algo errado!');
+    } else {
+      res.json(results);
+    }
+  });
+});
+
+app.get('/produtos/:id', function(req, res) {
+  produtos.findById(req.params.id, function(error, results) {
+    if(error) {
+      console.log(error);
+      res.status(500).send('Algo errado!');
+    } else if(results.length == 0) {
+      res.status(404).send("Produto Inexistente");
+    } else {
+      res.json(results);
+    }
+  });
+});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
